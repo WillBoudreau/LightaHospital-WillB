1748f833a343cd905e548191aac34c298c3a51ed
WillBoudreau <williamboudreau804@gmail.com>
2023-10-23 11:49:13 -0300

    Update 11:48pm on October 23rd 2023

---
+++ Assets/Scripts/StartNightTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class StartNightTrigger : MonoBehaviour
+{
+    public GameObject StartNight;
+    public Scene TimeStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartNight.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+       SceneManager.LoadScene(0);
+    }
+    public void ClickStartNight()
+    {
+        
+    }
+}
